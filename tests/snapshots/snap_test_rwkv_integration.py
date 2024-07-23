e22659d3ddc0e310d4358d1b22a72560bc735aee
Huanghe <xs28@rice.edu>
2024-07-23 17:53:55 -0500

    Add snapshots

---
+++ tests/snapshots/snap_test_rwkv_integration.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['test_rwkv_integration 1'] = '''一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个一个
+'''
