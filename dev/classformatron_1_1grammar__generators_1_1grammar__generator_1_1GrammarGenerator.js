be5331488e2e6e78b2624c3e995f4d2bafdf055b
Dan-wanna-M <55623775+Dan-wanna-M@users.noreply.github.com>
2024-08-07 23:28:37 +0000

    Deploying to gh-pages from @ Dan-wanna-M/formatron@30635e60553a2b8829e1406f0c4fda2d60d0842b 🚀

--- dev/classformatron_1_1grammar__generators_1_1grammar__generator_1_1GrammarGenerator.js
+++ dev/classformatron_1_1grammar__generators_1_1grammar__generator_1_1GrammarGenerator.js
@@ -1,5 +1,5 @@
 var classformatron_1_1grammar__generators_1_1grammar__generator_1_1GrammarGenerator =
 [
     [ "generate", "classformatron_1_1grammar__generators_1_1grammar__generator_1_1GrammarGenerator.html#a5c1c76bc0151686220be9902b064fe02", null ],
-    [ "get_extractor", "classformatron_1_1grammar__generators_1_1grammar__generator_1_1GrammarGenerator.html#af8697ae2e156443620146d97fe7868be", null ]
+    [ "get_extractor", "classformatron_1_1grammar__generators_1_1grammar__generator_1_1GrammarGenerator.html#ab4ada54b7c4a1d8396308d3da2bf022c", null ]
 ];
