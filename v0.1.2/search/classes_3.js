4299e2c9fe32b64eb5685abf11a965110db571f2
Dan-wanna-M <55623775+Dan-wanna-M@users.noreply.github.com>
2024-08-10 23:06:53 +0000

    Deploying to gh-pages from @ Dan-wanna-M/formatron@e33fca4aabedd44572b24f793823d7596aaf15e5 🚀

---
+++ v0.1.2/search/classes_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['grammargenerator_0',['GrammarGenerator',['../classformatron_1_1grammar__generators_1_1grammar__generator_1_1GrammarGenerator.html',1,'formatron::grammar_generators::grammar_generator']]]
+];
